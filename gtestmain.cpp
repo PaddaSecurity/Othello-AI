640690ce03e9f1b038e98eccbc28b8b5da5f3136
PaddaSecurity <50336501+PaddaSecurity@users.noreply.github.com>
2022-04-04 14:42:15 -0700

    Update gtestmain.cpp

--- gtestmain.cpp
+++ gtestmain.cpp
@@ -1,13 +1,3 @@
-// gtestmain.cpp
-//
-// ICS 46 Winter 2022
-// Project #3: Black and White
-//
-// DO NOT MODIFY THIS FILE AT ALL.  Its job is to launch Google Test and run
-// any unit tests that you wrote in source files in the "gtest" directory.
-// Simply add new source files to the "gtest" directory and write unit tests
-// in them and they should be picked up automatically the next time you
-// run gtest.
 
 #include <gtest/gtest.h>
 
